fc19660da0b40a8f0b352ebb65b5a66141e53dcb
Lukasz Tylski <lukasz.tylski@bdi.net.pl>
2024-01-08 02:03:25 -0500

    Change to find a string + fmt

--- heating.lua
+++ heating.lua
@@ -16,23 +16,23 @@ for i = 1,1000 do
   if devType == "com.fibaro.thermostatDanfoss" then
     roomId                         = fibaro:getRoomID(i);
     roomSet                        = fibaro:getValue(i, "value");
-  	dataRoomThermostat[roomId]     = i;
-  	dataRoomThermostatTemp[roomId] = roomSet;
+    dataRoomThermostat[roomId]     = i;
+    dataRoomThermostatTemp[roomId] = roomSet;
     fibaro: debug('Thermostat in ' .. roomId .. ' named ' .. fibaro:getName(i) .. ' set up to: ' .. roomSet);
   end
-  
+
   if devType == "virtual_device" then
-  	if fibaro:getName(i) == "VThermostat" then
+    if string.find(fibaro:getName(i), "VThermostat") then
       roomId                         = fibaro:getRoomID(i);
       roomSet                        = fibaro:getValue(i, "ui.Temperature.value");
-      dataRoomThermostat[roomId]     = i;    
+      dataRoomThermostat[roomId]     = i;
       dataRoomThermostatTemp[roomId] = roomSet;
       fibaro:debug('Virtual thermostat in ' .. roomId .. ' named ' .. fibaro:getName(i) .. ' set up to: ' .. roomSet);
     end
   end
-  
+
   if devType == "com.fibaro.temperatureSensor" then
-  	if string.find(fibaro:getName(i), '2', 1, true) == nil then
+    if string.find(fibaro:getName(i), '2', 1, true) == nil then
       roomId                     = fibaro:getRoomID(i);
       roomTemp                   = fibaro:getValue(i, "value");
       dataRoomTempSensor[roomId] = i;
@@ -52,24 +52,24 @@ end
 
 local roomRequiredTemp = 18; -- being set just in case of..,
 local roomCurrentTemp  = 18; -- being set just in case of..,
-local co_seton         = 0;  
+local co_seton         = 0;
 local roomName;
 
-for k, v in pairs(dataRoomThermostatTemp) do 
-  	roomRequiredTemp = v + hist;
-  	roomCurrentTemp  = tonumber(dataRoomTemp[k]);  
+for k, v in pairs(dataRoomThermostatTemp) do
+    roomRequiredTemp = v + hist;
+    roomCurrentTemp  = tonumber(dataRoomTemp[k]);
     co_status        = tonumber(fibaro:getGlobalValue('CO_STATUS'));
     roomName         = fibaro:getRoomName(k);
-  	fibaro:debug('Room ' .. roomName .. ' Required(' .. dataRoomThermostat[k] .. ') ' .. roomRequiredTemp .. ' Current(' .. dataRoomTempSensor[k] .. ') ' .. roomCurrentTemp .. ' CO Status ' .. co_status);
+    fibaro:debug('Room ' .. roomName .. ' Required(' .. dataRoomThermostat[k] .. ') ' .. roomRequiredTemp .. ' Current(' .. dataRoomTempSensor[k] .. ') ' .. roomCurrentTemp .. ' CO Status ' .. co_status);
 
-  	if roomCurrentTemp < roomRequiredTemp and co_status == 1 then
-    	co_seton = 1;
-    	table.insert(roomsToHeat, roomName);
-    	fibaro:debug('Still On');
-   	elseif roomCurrentTemp < roomRequiredTemp and co_status == 0 then
-    	co_seton = 1;
-    	table.insert(roomsToHeat, roomName);
-    	fibaro:debug('Turn On');    
+  if roomCurrentTemp < roomRequiredTemp and co_status == 1 then
+    co_seton = 1;
+    table.insert(roomsToHeat, roomName);
+    fibaro:debug('Still On');
+  elseif roomCurrentTemp < roomRequiredTemp and co_status == 0 then
+    co_seton = 1;
+    table.insert(roomsToHeat, roomName);
+    fibaro:debug('Turn On');
     end
 end
 
@@ -79,20 +79,20 @@ if tonumber(fibaro:getGlobalValue('SUMMER')) == 1 then
 end
 
 if co_seton == 1 then
-	fibaro:debug('Should be set On');
-	fibaro:setGlobal('CO_STATUS', 1);
+  fibaro:debug('Should be set On');
+  fibaro:setGlobal('CO_STATUS', 1);
 elseif co_seton == 0 then
-	fibaro:debug('Turn Off');
-	fibaro:setGlobal('CO_STATUS', 0);
+  fibaro:debug('Turn Off');
+  fibaro:setGlobal('CO_STATUS', 0);
 end
 
 co_status = tonumber(fibaro:getGlobalValue('CO_STATUS'));
 fibaro:debug('New CO status ' .. co_status);
 
 if co_status == 1 then
-	fibaro:call(device, "setProperty", "ui.LabelStatus.value", "On");
-	fibaro:call(device, "setProperty", "ui.LabelRoomsToHeat.value", table.concat(roomsToHeat, ", "));
+  fibaro:call(device, "setProperty", "ui.LabelStatus.value", "On");
+  fibaro:call(device, "setProperty", "ui.LabelRoomsToHeat.value", table.concat(roomsToHeat, ", "));
 elseif co_status == 0 then
-	fibaro:call(device, "setProperty", "ui.LabelStatus.value", "Off");
-        fibaro:call(device, "setProperty", "ui.LabelRoomsToHeat.value", "NONE");
-end  
+  fibaro:call(device, "setProperty", "ui.LabelStatus.value", "Off");
+  fibaro:call(device, "setProperty", "ui.LabelRoomsToHeat.value", "NONE");
+end
